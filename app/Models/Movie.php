99d27a12ab0b6435586cc5c1ec7fa43f5f3988ec
Diego Moura <diego.moura@theideaofnow.com>
2024-02-10 09:25:48 -0300

    add soft delete trait to movie model

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Movie extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['title','description','cover','release_date', 'duration'];
 
