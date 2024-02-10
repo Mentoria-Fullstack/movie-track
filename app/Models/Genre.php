75e9576043dec9f1d0ba223fd8e46141b9b94ae2
Diego Moura <diego.moura@theideaofnow.com>
2024-02-10 07:53:31 -0300

    first commit

---
+++ app/Models/Genre.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Genre extends Model
+{
+    use HasFactory;
+
+
+    public function movies(): BelongsTo
+    {
+        return $this->belongsToMany(Movie::class);
+    }
+}
---
+++ database/migrations/2024_01_27_115829_create_tv_shows_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tv_shows', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('cover');
+            $table->date('release_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tv_shows');
+    }
+};
