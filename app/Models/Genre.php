30870d4cf60954f4415692a4de69763cd35ab910
Heinrich Mises <henrique.lapf@gmail.com>
2024-02-16 17:04:41 -0300

    fix genre belongs to many

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Genre extends Model
 {
     use HasFactory;
 
 
-    public function movies(): BelongsTo
+    public function movies(): BelongsToMany
     {
         return $this->belongsToMany(Movie::class);
     }
